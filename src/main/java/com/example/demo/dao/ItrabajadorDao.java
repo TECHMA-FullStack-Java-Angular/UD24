48d13ab955d9e97c631bfafaefb9b065d3b21119
Paula Sousa <paulasousa137@gmail.com>
2023-04-13 11:29:12 +0200

    Interface DAO trabajador

---
+++ src/main/java/com/example/demo/dao/ItrabajadorDao.java
@@ -0,0 +1,8 @@
+package com.example.demo.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.dto.Trabajador;
+
+public interface ItrabajadorDao extends JpaRepository<Trabajador, Long>{
+
+}
